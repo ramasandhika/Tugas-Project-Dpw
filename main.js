f5173bf2f172b58b1e065504675659ff04147c4c
Zey21 <77796843+Zey21@users.noreply.github.com>
2021-05-15 17:26:27 +0700

    Update main.js
    Penambahan fungsi pembilang

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-
+function pembilang(Num){
+    var angka = ["","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh","Delapan","Sembilan","Sepuluh","Sebelas"];
+    var hasil = "" ;
+    var n = parseInt(Num) ;
+    if(n >- 0, n <= 11){
+        hasil = angka[n]
+    } else if (n < 20) {
+        hasil = pembilang(n -10) + " Belas ";
+    } else if (n < 100) {
+        hasil = pembilang(n / 10) + " Puluh " + pembilang(n % 10);
+    } else if (n < 200) {
+        hasil = "Seratus " + pembilang(n - 100);
+    } else if (n < 1000) {
+        hasil = pembilang(n / 100) + " Ratus " + pembilang(n % 100);
+    } else if (n < 2000) {
+        hasil = " Seribu " + pembilang(n - 1000);
+    } else if (n < 1000000) {
+        hasil = pembilang(n / 1000) + " Ribu " + pembilang(n % 1000);
+    } else if (n < 1000000000) {
+        hasil = pembilang(n / 1000000) + " Juta " + pembilang(n % 1000000);
+    } else if (n < 1000000000000) {
+        hasil = pembilang( n / 1000000000) + " Milyar " + pembilang(n % 1000000000);
+    } else if (n < 1000000000000000) {
+        hasil = pembilang(n / 1000000000000) + " Triliyun " + pembilang(n % 1000000000000);
+    } else if (n == 1000000000000000) {
+        hasil = "Satu Kuadriliun" ;
+    } else {
+        hasil = "Angka Hanya Sampai Satu Kuadriliun" ;
+    }
+    return hasil
+}
